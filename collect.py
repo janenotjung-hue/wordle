c05cda93669c2d38eaad0432c9a592204bbeab2e
janenotjung-hue <107402425+janenotjung-hue@users.noreply.github.com>
2024-05-18 14:57:25 +1200

    commit before doing 300k ep

--- collect.py
+++ collect.py
@@ -3,46 +3,57 @@ import gymnasium as gym
 import pandas as pd
 from Wordle import WordleEnv
 from stable_baselines3.common.env_checker import check_env
+from collections import namedtuple
+from imitation.data.rollout import flatten_trajectories_with_rew
+import imitation.data.types as dt
+from imitation.util import util
+from typing import Mapping, Sequence, cast
+Transition = namedtuple('Transition',
+                        ('state', 'action', 'next_state', 'reward'))
 
-gym.register(
-    id='WordleGame-v0',
-    entry_point='Wordle:WordleEnv',
-    max_episode_steps=7
-)
-env = gym.make('WordleGame-v0')
+
+env = WordleEnv()
 #check_env(env)
-num_episodes = 10
+num_episodes = 1
 with open('data/wordle_actual.txt', 'r') as f:
     words = [word.strip().upper() for word in f.readlines() if len(word.strip()) == 5]
 
 df = pd.read_csv('dataset/normal.csv')
 guesses = df.drop(columns=["hits_0","hits_1","hits_2","hits_3","hits_4"], axis=1)
 guesses = guesses.replace("     ", None)
-values = []
+answers = []
 for index, row in df.iterrows():
     for i in range(0, len(row)):
         if i < len(row) and row.iloc[i] == 'GGGGG':
-            values.append(row.iloc[i-1])
+            answers.append(row.iloc[i-1])
+
+import torch
+from ReplayMemory import ReplayMemory
+memory = []
 
 for episode in range(num_episodes):
+    print(episode)
     state = env.reset()
     row = guesses.iloc[episode]
-    env.target_word = values[episode]
+    env.target_word = answers[episode].upper()
     for index, value in row.items():
         if value is not None:
             action = words.index(value.upper())
-            observation, reward, done, _ = env.step(action)
+            observation, reward, done, done, _ = env.step(action)
+            reward = torch.tensor([reward])
             env.render()
             if done:
                 next_state = None
             else:
                 next_state = observation
+                next_state = torch.tensor(observation, dtype=torch.float32)
+                #TODO: look at how to add trajectory? transition?
+            memory.append(state, action, next_state, reward)
             if done:
                 break
-            action = value
             state = next_state
         
-    
+rollout = flatten_trajectories_with_rew(memory)
 print('Complete')
 
 
--- pretrain.py
+++ pretrain.py
@@ -14,11 +14,11 @@ from imitation.data import rollout
 from imitation.data.wrappers import RolloutInfoWrapper
 from stable_baselines3.common.vec_env import DummyVecEnv
 import numpy as np
-
+# https://imitation.readthedocs.io/en/latest/_api/imitation.data.huggingface_utils.html#imitation.data.huggingface_utils.TrajectoryDatasetSequence look at this for link
 gym.register(
     id='WordleGame-v0',
     entry_point='Wordle:WordleEnv',
-    max_episode_steps=7
+    max_episode_steps=6
 )
 
 env = gym.make('WordleGame-v0')
@@ -35,13 +35,12 @@ from stable_baselines3.common.evaluation import evaluate_policy
 
 expert = PPO(
     policy=MlpPolicy,
-    env=env,
-    seed=0,
-    batch_size=64,
+    env=venv,
+    batch_size=6,
     ent_coef=0.0,
     learning_rate=0.0003,
     n_epochs=10,
-    n_steps=64,
+    n_steps=6,
 )
 
 #have a look at the section about transitions
@@ -50,11 +49,11 @@ https://imitation.readthedocs.io/en/latest/tutorials/1_train_bc.html
 Note that the rollout function requires a vectorized environment and needs the RolloutInfoWrapper around each of the environments. This is why we passed the post_wrappers argument to make_vec_env above.
 """
 
-reward, _ = evaluate_policy(expert, env, 10)
+reward, _ = evaluate_policy(expert, venv, 10)
 print(f"Reward before training: {reward}")
     
-expert.learn(100)  # Note: set to 100000 to train a proficient expert
+expert.learn(10)  # Note: set to 300000 to train a proficient expert
 
-expert.save('ppo_expert.zip')
-reward, _ = evaluate_policy(expert, expert.get_env(), 10)
+expert.save('ppo_expert_300k.zip')
+reward, _ = evaluate_policy(expert, venv, 10)
 print(f"Expert reward: {reward}")
